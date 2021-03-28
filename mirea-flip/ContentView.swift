d7d58f99f978650d19a8768f62a7fc823bc437cd
defaulterrr <thedefaulterrr@gmail.com>
2021-03-29 00:38:44 +0300

    Initial Commit

---
+++ mirea-flip/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  mirea-flip
+//
+//  Created by defaulterrr on 29.03.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
